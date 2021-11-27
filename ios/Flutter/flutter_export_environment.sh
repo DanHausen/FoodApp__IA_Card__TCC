98d6fa007ab1857de836870d6053deb18e9a8097
Danhausen <danihausen@live.com>
2021-11-26 21:52:36 -0300

    Update

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\GitHub\FATEC\FoodApp__IA_Card__TCC"
+export "FLUTTER_ROOT=C:\tools\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\danih\Documents\Github\Flutter\FoodApp__IA_Card__TCC"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
