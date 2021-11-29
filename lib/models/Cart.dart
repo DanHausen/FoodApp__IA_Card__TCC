bdf67e49311c43230a02e6d1313b9c9b5ecf014e
Danhausen <danihausen@live.com>
2021-11-28 23:08:24 -0300

    woi

--- lib/models/Cart.dart
+++ lib/models/Cart.dart
@@ -2,7 +2,36 @@ import 'package:flutter/material.dart';
 
 import 'Product.dart';
 
-// class ShopCart {
-//   final Product product;
-//   final int numOfProducts;
-// }
+class ShopCart {
+  String key, name, image, price;
+  int quantity;
+  double totalPrice;
+
+  ShopCart({
+    this.key,
+    this.name,
+    this.image,
+    this.price,
+    this.quantity,
+    this.totalPrice,
+  });
+
+  ShopCart.fromJSON(Map<String, dynamic> json) {
+    key = json['key'];
+    name = json['name'];
+    image = json['image'];
+    price = json['price'];
+    quantity = json['quantity'];
+    totalPrice = json['totalPrice'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['key'] = this.key;
+    data['name'] = this.key;
+    data['image'] = this.key;
+    data['price'] = this.key;
+    data['quantity'] = this.key;
+    data['totalPrice'] = this.key;
+  }
+}
