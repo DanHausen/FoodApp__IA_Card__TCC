e4436aa3cb801ec1229294d54e2c3f420d25437a
Danhausen <danihausen@live.com>
2021-11-30 19:51:35 -0300

    woi

--- lib/models/Cart.dart
+++ lib/models/Cart.dart
@@ -1,14 +1,19 @@
 class Cart {
-  static var cartList = {};
+  static var cartList = new Map();
 
   static var item = new Map();
+  static int x = 0;
 
   static void ItemAddToCart(String name, var qtdeItems) {
     item = {
       'Name': name,
       'Quantidade': qtdeItems,
     };
-    Cart.cartList = item;
-    print(Cart.cartList);
+    Cart.cartList[x] = item;
+    x++;
+    // Cart.cartList.forEach((key, value) {
+    //   print(key);
+    // });
+    print(cartList);
   }
 }
