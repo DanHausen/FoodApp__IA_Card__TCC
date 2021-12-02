828a8a14eca5f17b14f711b96750acb56ad94c69
Danhausen <danihausen@live.com>
2021-12-01 22:45:41 -0300

    firebase_json_init

--- lib/data/teste_drink.dart
+++ lib/data/teste_drink.dart
@@ -1,4 +1,6 @@
 import 'package:ia_card/models/drink.dart';
+import 'dart:convert';
+
 
 const TESTE_DRINK = {
   '1': const Drink(
@@ -10,7 +12,6 @@ const TESTE_DRINK = {
     image:
         'https://d3pt1seq4txask.cloudfront.net/Custom/Content/Products/01/01/0101_refrigerante-coca-cola-lata-350ml-14_m6_637417305699974110.jpg',
   ),
-
   '2': const Drink(
     id: 2,
     name: 'Guaraná',
@@ -20,17 +21,6 @@ const TESTE_DRINK = {
     image:
         'https://formulachopp.cdn.3techstore.com.br/img/p/1/1/6/8/6/11686-thickbox_default.jpg',
   ),
-
-//   '3': const Drink(
-//    id: 3,
-//    name: 'Suco de Laranja',
-//    value: '8.00',
-//    desc: 'Suco Natural 400ml',
-//    filtros: "Sem glúten",
-//    image: 'https://easyminimercado.com.br/wp-content/uploads/2021/05/laranja.jpg',
-
-//  ),
-
   '3': const Drink(
     id: 4,
     name: 'Suco de Uva',
