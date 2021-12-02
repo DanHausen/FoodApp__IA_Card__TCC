d045070be5f17c3a4101d5c4cf3b15c1fcb5a47f
Danhausen <danihausen@live.com>
2021-12-02 11:41:07 -0300

    Apresentacao

--- lib/pages/Product_page.dart
+++ lib/pages/Product_page.dart
@@ -7,7 +7,7 @@ import 'package:ia_card/pages/cart_page.dart';
 import 'package:ia_card/widgets/Product_tile.dart';
 
 class ProductPageClass extends StatefulWidget {
-  ProductPageClass({this.name}); //TODO deixar o código melhor
+  ProductPageClass({this.name});
   final String name;
 
   @override
@@ -18,35 +18,39 @@ class _ProductPageClassState extends State<ProductPageClass> {
   final fb = FirebaseDatabase.instance.reference().child("Products");
   final List<Product> list = [];
   static var productList = {};
+  int n = 01;
 
   @override
   void initState() {
     super.initState();
     fb.once().then((DataSnapshot snap) {
       var data = snap.value;
-      data.forEach((key, value) {
-        Product product = new Product(
-          id: value["01"]["id"],
-          name: value["01"]["name"],
-          filtros: value["01"]["filtros"],
-          price: value["01"]["price"],
-          description: value["01"]["description"],
-          image: value["01"]["image"],
-          key: key,
-        );
-        productList[key] = [
-          [
-            product.id,
-            product.name,
-            product.filtros,
-            product.price,
-            product.description,
-            product.image,
-            product.key
-          ]
-        ];
-        list.add(product);
-      });
+      list.clear();
+      data.forEach(
+        (key, value) {
+          Product product = new Product(
+            id: value[n.toString().padLeft(n + 1, '0')]["id"],
+            name: value[n.toString().padLeft(n + 1, '0')]["name"],
+            filtros: value[n.toString().padLeft(n + 1, '0')]["filtros"],
+            price: value[n.toString().padLeft(n + 1, '0')]["price"],
+            description: value[n.toString().padLeft(n + 1, '0')]["description"],
+            image: value[n.toString().padLeft(n + 1, '0')]["image"],
+            key: key,
+          );
+          productList[key] = [
+            [
+              product.id,
+              product.name,
+              product.filtros,
+              product.price,
+              product.description,
+              product.image,
+              product.key
+            ]
+          ];
+          list.add(product);
+        },
+      );
       setState(() {});
     });
   }
@@ -63,7 +67,6 @@ class _ProductPageClassState extends State<ProductPageClass> {
             icon: Icon(Icons.shopping_basket, size: 35),
             color: Colors.white,
             onPressed: () {
-              print(list[1].image);
               Navigator.push(
                 context,
                 MaterialPageRoute(builder: (context) => CartPage()),
@@ -72,7 +75,7 @@ class _ProductPageClassState extends State<ProductPageClass> {
           ),
         )
       ],
-      title: new Text('Bebidas',
+      title: new Text(widget.name,
           style: GoogleFonts.passionOne(
               fontStyle: FontStyle.normal, fontSize: 40)),
       backgroundColor: Color.fromRGBO(255, 161, 73, 1),
@@ -92,9 +95,11 @@ class _ProductPageClassState extends State<ProductPageClass> {
     return new Scaffold(
       appBar: searchBar.build(context),
       body: ListView.builder(
-        itemCount: productList["Drink"].length,
-        itemBuilder: (ctx, i) => ProductTile(list[i]),
-      ),
+          itemCount: productList[widget.name].length,
+          itemBuilder: (ctx, i) {
+            //TODO Código index em falha
+            return ProductTile(list[1]);
+          }),
     );
   }
 }
