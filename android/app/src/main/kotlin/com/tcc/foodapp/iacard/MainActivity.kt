cb2aeab05d95f42e99886e8c3df7df08c2571804
Danhausen <danihausen@live.com>
2022-05-11 11:59:36 -0300

    update-gitignore

--- android/app/src/main/kotlin/com/tcc/foodapp/iacard/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.tcc.foodapp.iacard
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/constants.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-const kPrimaryColor = Colors.orange;
-const kPrimaryLightColor = Color.fromRGBO(255, 161, 73, 1);
-const kSecontaryColor = Colors.white;
-const kTextColor = Colors.grey;
-
-const kAnimationDuration = Duration(milliseconds: 200);
